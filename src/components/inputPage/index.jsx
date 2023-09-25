dc3ac3ad53fcbe09b1ff80c5ba84a002f8889a97
Nishank Priydarshi <nishankpr2002@gmail.com>
2023-09-25 16:49:05 +0530

    intial

---
+++ src/components/inputPage/index.jsx
@@ -0,0 +1,11 @@
+import styles from "./styles.module.scss";
+
+const InputPage = () => {
+    return (
+        <div className={styles.box}>
+            Hello
+        </div>
+    )
+}
+
+export default InputPage
---
+++ src/pages/inputsPage/index.jsx
@@ -0,0 +1,7 @@
+import InputPage from "../../components/inputPage"
+
+const InputsPage = () => {
+    return <InputPage />
+}
+
+export default InputsPage
